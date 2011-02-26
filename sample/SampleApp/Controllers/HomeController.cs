890d4494091b2ef53fea78f7a2f680f72a3be7e7
Andrew Davey <andrew@equin.co.uk>
2011-02-26 12:13:24 +0000

    Add sample project.

---
+++ sample/SampleApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using NotFoundMvc;
+
+namespace SampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test(int id)
+        {
+            return new NotFoundViewResult();
+        }
+    }
+}

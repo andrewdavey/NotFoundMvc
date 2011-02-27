9d0a584c4a826f320409b4f8f2314c2dd1d7740c
Brandon Kaid <brandonkaid@gmail.com>
2011-02-27 15:47:16 -0700

    Added NotFoundHandler, a HTTP handler to use in the Views/Web.config instead of System.Web.HttpNotFoundHandler to block access to files in the /Views folder and show your custom 404 page instead of the ASP.NET 404 page. NuGet packages don't support full Web.config transformations (vote for it at http://nuget.codeplex.com/workitem/232) so installation is manual - by editing /Views/Web.config yourself.
    Signed-off-by: Brandon Kaid <brandonkaid@gmail.com>

---
+++ src/NotFoundMvc/NotFoundHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NotFoundMvc
+{
+    public class NotFoundHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "NotFound");
+            var controllerContext = new ControllerContext(new HttpContextWrapper(context), routeData, new FakeController());
+            var notFoundViewResult = new NotFoundViewResult();
+            notFoundViewResult.ExecuteResult(controllerContext);
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        // ControllerContext requires an object that derives from ControllerBase.
+        class FakeController : Controller { }
+    }
+}

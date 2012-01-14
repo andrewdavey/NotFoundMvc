dd3fe533fb189fc941bea379162e882748a6caf1
Andrew Davey <andrew@equin.co.uk>
2012-01-14 11:06:53 +0000

    Add area to SampleApp project. Also clean up code.

---
+++ sample/SampleApp/Areas/Test/Controllers/ExampleController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace SampleApp.Areas.Test.Controllers
+{
+    public class ExampleController : Controller
+    {
+        //
+        // GET: /Test/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ sample/SampleApp/Areas/Test/TestAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SampleApp.Areas.Test
+{
+    public class TestAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Test";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Test_default",
+                "Test/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

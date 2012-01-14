cda8b962544e53a96182eb474c89093938117a4f
Andrew Davey <andrew@equin.co.uk>
2012-01-14 10:46:23 +0000

    Change base class of NotFoundViewResult to HttpNotFoundResult - fixes issue #6

--- src/NotFoundMvc/NotFoundViewResult.cs
+++ src/NotFoundMvc/NotFoundViewResult.cs
@@ -6,12 +6,19 @@ namespace NotFoundMvc
     /// Renders a view called "NotFound" and sets the response status code to 404.
     /// View data is assigned for "RequestedUrl" and "ReferrerUrl".
     /// </summary>
-    public class NotFoundViewResult : ViewResult
+    public class NotFoundViewResult : HttpNotFoundResult
     {
         public NotFoundViewResult()
         {
             ViewName = "NotFound";
+            ViewData = new ViewDataDictionary();
         }
+
+        /// <summary>
+        /// The name of the view to render. Defaults to "NotFound".
+        /// </summary>
+        public string ViewName { get; set; }
+        public ViewDataDictionary ViewData { get; set; }
 
         public override void ExecuteResult(ControllerContext context)
         {
@@ -19,16 +26,22 @@ namespace NotFoundMvc
             var request = context.HttpContext.Request;
             var url = request.Url.OriginalString;
 
+            var viewResult = new ViewResult
+            {
+                ViewName = ViewName,
+                ViewData = ViewData
+            };
+
             ViewData["RequestedUrl"] = url;
             ViewData["ReferrerUrl"] = (request.UrlReferrer != null && request.UrlReferrer.OriginalString != url)
                                         ? request.UrlReferrer.OriginalString 
                                         : null;
-
+            
             response.StatusCode = 404;
             // Prevent IIS7 from overwriting our error page!
             response.TrySkipIisCustomErrors = true;
-
-            base.ExecuteResult(context);
+            
+            viewResult.ExecuteResult(context);
         }
     }
 }

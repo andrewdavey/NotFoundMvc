fe362aa2f30ba0250c1300764db76837f69c6f7c
Andrew Davey <andrew@equin.co.uk>
2013-05-19 11:20:41 +0100

    Update nuget and fix build

--- package/build.cmd
+++ package/build.cmd
@@ -3,4 +3,5 @@ del /Q input\lib\net40\*.*
 
 msbuild ..\src\NotFoundMvc\NotFoundMvc.csproj /p:Configuration=Release;OutputPath=..\..\package\input\lib\net40
 
+mkdir output
 ..\tools\nuget.exe pack /o output input\notfoundmvc.nuspec

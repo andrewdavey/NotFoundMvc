b7f340cf5f4c815d0ed5b37a9a1fd0c31c6a4055
David De Sloovere <david.desloovere@outlook.com>
2015-10-15 18:13:01 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -13,10 +13,10 @@ if not "%PackageVersion%" == "" (
 
 set nuget=
 if "%nuget%" == "" (
-    set nuget=nuget.exe
+    set nuget=nuget
 )
 
-ECHO ** Package restore
+ECHO ** Package restore w/ %nuget%
 cmd /c %nuget% restore "src\build.sln"
 
 ECHO ---
@@ -24,6 +24,6 @@ ECHO ** Build / %config%
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "src\build.sln" /t:rebuild /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=normal /nr:false
 
 ECHO ---
-ECHO ** Package w/ %nuget% / version: %version%
+ECHO ** Package pack w/ %nuget% / version: %version%
 mkdir Build
 cmd /c %nuget% pack "src\NotFoundMvc\NotFoundMvc.csproj" -symbols -o Build -p Configuration=%config% %version%
